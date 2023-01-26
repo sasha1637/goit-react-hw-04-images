f1cf4ef3fd6b978ef2f2ae46bdee2420c7f24e3a
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2023-01-26 16:07:54 +0200

    Update Button.styled.js

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 export const ButtonLoadMore = styled.button`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
   padding: 8px 16px;
   border-radius: 2px;
   background-color: #3f51b5;
